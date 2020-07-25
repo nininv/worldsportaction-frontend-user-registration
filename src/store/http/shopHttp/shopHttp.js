fbaa73df3ca5c0ec34721a4b3efbac51261ea17f
Sukhdeep Singh <sukhdeep.82916@gmail.com>
2020-07-25 18:06:54 +0530

    Env File and shop route updated

--- src/store/http/shopHttp/shopHttp.js
+++ src/store/http/shopHttp/shopHttp.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL: "https://shop-api-dev.worldsportaction.com"
+    baseURL: process.env.REACT_APP_SHOP_API_URL
 });
 
 http.interceptors.request.use(function (config) {
