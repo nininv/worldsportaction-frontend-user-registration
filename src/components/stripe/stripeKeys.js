61c4d4d5729a04e493993ede1ed283bcc19019b7
Gladson <gladson@revolite.io>
2020-10-17 12:04:34 +0530

    Stripe Keys Update

--- src/components/stripe/stripeKeys.js
+++ src/components/stripe/stripeKeys.js
@@ -1,18 +1,10 @@
-let devStripeKeys = {
-    publicKey: "pk_test_51GucJDEnewRwSTgnqNBzaTR1MD6u6N9wqFTNgFtWXQHAxx8bOnQAnSZQkpptj4Quyc8CEwNw1ZBFk8X9dTTs5aOx00FWRr6ClF",
-    clientId: "ca_HTZksXTH0PEabfP0wIh7KG9VRCQZcUsu",
-    url: "https://netball-comp-admin-dev.worldsportaction.com",////////dev server
-    apiURL: "https://registration-api-dev.worldsportaction.com",
-};
-
-let stgStripeKeys = {
+let stripeKeys = {
     publicKey: "pk_test_mxxrmGxL3Z0FoKhELpnDQykk007volnWs2",
     clientId: "ca_GciWEdWxJlRdyKn5pJN8ogDDCmMC2Rof",
-    url: "https://netball-comp-admin-stg.worldsportaction.com",////staging server
+    url: "https://netball-registration-stg.worldsportaction.com/userPersonal",
     apiURL: "https://netball-api-stg.worldsportaction.com/registration",
 };
 
-//const StripeKeys = devStripeKeys  ////dev server
- const StripeKeys = stgStripeKeys   ///staging server
+ const StripeKeys = stripeKeys  
 
 export default StripeKeys;
