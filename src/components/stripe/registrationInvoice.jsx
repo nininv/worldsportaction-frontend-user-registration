97c6a93675ef4cfb65594c7617cd0b04782c12a4
Gladson <gladson@revolite.io>
2020-11-24 20:49:40 +0530

    Team Name fix

--- src/components/stripe/registrationInvoice.jsx
+++ src/components/stripe/registrationInvoice.jsx
@@ -50,12 +50,12 @@ class RegistrationInvoice extends Component {
 
     getInvoiceStatusAPI = () => {
        // console.log("this.props.location.state.registrationId" + this.props.location.state.registrationId);
-       let registrationId = this.props.location.state ? this.props.location.state.registrationId : null;
-       let userRegId = this.props.location.state ? this.props.location.state.registrationId : null;
-       let invoiceId = this.props.location.state ? this.props.location.state.invoiceId : null;
-    //    let registrationId = "25955e35-b2ae-412f-bed3-4a104f4e4348";
-    //    let userRegId = null;
-    //    let invoiceId = null;
+    //    let registrationId = this.props.location.state ? this.props.location.state.registrationId : null;
+    //    let userRegId = this.props.location.state ? this.props.location.state.registrationId : null;
+    //    let invoiceId = this.props.location.state ? this.props.location.state.invoiceId : null;
+       let registrationId = null;
+       let userRegId = "53673c37-7729-49f4-ba31-ba3a231a4e03";
+       let invoiceId = null;
        this.props.getInvoiceStatusAction(registrationId, userRegId, invoiceId);
         //this.props.getInvoiceStatusAction('05c59bfc-9438-42e6-8917-4a60ed949281')
         this.setState({ checkStatusLoad: true });
@@ -75,12 +75,12 @@ class RegistrationInvoice extends Component {
         if (stripeState.onLoad == false && this.state.checkStatusLoad === true) {
             this.setState({ checkStatusLoad: false });
            // let invoiceId = this.props.stripeState.invoiceId
-            let registrationId = this.props.location.state ? this.props.location.state.registrationId : null;
-            let userRegId = this.props.location.state ? this.props.location.state.userRegId : null;
-            let invoiceId = this.props.location.state ? this.props.location.state.invoiceId : null;
-            //  let registrationId = "25955e35-b2ae-412f-bed3-4a104f4e4348";
-            //  let userRegId = null;
-            //  let invoiceId = null;
+            // let registrationId = this.props.location.state ? this.props.location.state.registrationId : null;
+            // let userRegId = this.props.location.state ? this.props.location.state.userRegId : null;
+            // let invoiceId = this.props.location.state ? this.props.location.state.invoiceId : null;
+            let registrationId = null;
+            let userRegId = "53673c37-7729-49f4-ba31-ba3a231a4e03";
+            let invoiceId = null;
             this.props.getInvoice(registrationId, userRegId, invoiceId)
             //this.props.getInvoice('05c59bfc-9438-42e6-8917-4a60ed949281', invoiceId)
         }
@@ -557,7 +557,7 @@ class RegistrationInvoice extends Component {
                                                     {item && item.firstName &&
                                                         <InputWithHead
                                                             heading=
-                                                            {isTeamReg == 1 ?
+                                                            {isTeamReg == 1 || item.isTeamRegistration == null ?
                                                                 mem.divisionName ?
                                                                     regName + " - " + typeName + " " + mem.firstName + " " + mem.lastName
                                                                     + ", Team - " + item.teamName + ", " + item.competitionName + " - "+ mem.divisionName
