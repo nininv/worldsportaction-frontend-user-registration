9c58339ccc2ac651a6f5eb251154861bf0d7f8c4
Samir <samir@deftsoft.info>
2020-12-24 13:18:51 +0530

    CM-1871

--- src/components/registration/teamInvitePayment.jsx
+++ src/components/registration/teamInvitePayment.jsx
@@ -124,11 +124,12 @@ const CheckoutForm = (props) => {
             if (event.complete) {
                 if (elements) {
                     const card = elements.getElement(CardElement);
-                    if (card) {
+                    if (card != undefined) {
+                        console.log("card", card)
                         const cardToken = await stripe.createToken(card);
-                        console.log("cardToken", cardToken.token.card.country);
                         if (cardToken.token == undefined) {
-                            message.error(cardToken.error.message)
+                            message.error(cardToken.error.message);
+                            setError(cardToken.error.message);
                         }
                         else if (cardToken.token != undefined) {
                             const country = cardToken.token.card.country;
