5a76c439a6999fa6cba11b0c33bc57e27689d7bc
AsmaS <asma@worldsportaction.com>
2021-01-05 20:27:05 +1100

    clean up code

--- src/util/DocService.js
+++ src/util/DocService.js
@@ -3,7 +3,6 @@ import { savePDF } from '@progress/kendo-react-pdf';
 class DocService {
   createPdf = (html) => {
     savePDF(html, {
-      paperSize: 'Letter',
       fileName: 'invoice.pdf',
       scale: 0.6,
       paperSize: "A4",
