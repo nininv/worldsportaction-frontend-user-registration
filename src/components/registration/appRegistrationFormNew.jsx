8592c6e4d9c99cb28cde1062b8cc858262067fa7
samir <samir@deftsoft.info>
2021-01-06 13:41:39 +0530

    CM-1958

--- src/components/registration/appRegistrationFormNew.jsx
+++ src/components/registration/appRegistrationFormNew.jsx
@@ -614,11 +614,22 @@ class AppRegistrationFormNew extends Component {
                 });
             });
             if (key == 'referParentEmail' && value == true) {
+
                 this.setState({
                     sameEmailValidationModalVisible: true
                 })
             }
+
             if (key == "dateOfBirth") {
+
+                if (registrationObj != null) {
+                    setTimeout(() => {
+                        this.setState({
+                            sameEmailValidationModalVisible: registrationObj.referParentEmail
+                        })
+                    }, 800);
+                }
+
                 if (getAge(value) < 18) {
                     if (!isArrayNotEmpty(registrationObj.parentOrGuardian)) {
                         this.addParent("add");
@@ -3836,15 +3847,18 @@ class AppRegistrationFormNew extends Component {
                             {AppConstants.cancel}
                         </Button>,
                         <Button
+                            htmlType="submit"
+                            type="primary"
                             className="other-info-btn color-white"
                             onClick={() => {
                                 this.onChangeSetParticipantValue(true, "referParentEmail");
                                 this.setState({ sameEmailValidationModalVisible: false, hasChecked: false })
-                                this.stepNavigation(registrationObj, expiredRegistration);
-                                setTimeout(() => {
-                                    this.setState({ submitButtonText: AppConstants.addCompetitionAndMembership });
-                                }, 100);
-                            }}>
+                                // this.stepNavigation(registrationObj, expiredRegistration);
+                                // setTimeout(() => {
+                                //     this.setState({ submitButtonText: AppConstants.addCompetitionAndMembership });
+                                // }, 100);
+                            }}
+                        >
                             {AppConstants.continue}
                         </Button>
                     ]}
