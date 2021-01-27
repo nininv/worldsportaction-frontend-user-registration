eccd4c7232a3b95c9758b7970ef7d5fa29485f35
exis <gotexis@gmail.com>
2021-01-27 17:21:15 +1100

    add helper function

---
+++ src/util/lenientCompare.js
@@ -0,0 +1,7 @@
+export const lenientStrCompare = (a, b) => {
+    return (a || '').trim().toLowerCase() === (b || '').trim().toLowerCase()
+}
+
+export const lenientObjectCompare = (a, b, fields) => {
+    return fields.every((field) => lenientStrCompare(a[field], b[field]))
+}
