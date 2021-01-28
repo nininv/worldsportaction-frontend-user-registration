14510ac8316e079b77530cd04f1f04173838df60
exis <gotexis@gmail.com>
2021-01-29 10:46:06 +1100

    add steps

--- src/components/registration/appRegistrationFormNew/Validation.jsx
+++ src/components/registration/appRegistrationFormNew/Validation.jsx
@@ -8,9 +8,16 @@ export const UserValidation = () => {
     const [step, setStep] = useState(0);
     return (
         <>
-            {step === 1 && <UserAlreadyExists cancel={setStep(0)} />}
-            {step === 2 && <ConfirmDetails cancel={setStep(0)} />}
-            {step === 3 && <EnterCode cancel={setStep(0)} />}
+            {step === 1 && (
+                <UserAlreadyExists cancel={setStep(0)} next={setStep(2)} />
+            )}
+            {step === 2 && (
+                <ConfirmDetails cancel={setStep(0)} next={setStep(3)} />
+            )}
+            {step === 3 && <EnterCode cancel={setStep(0)} next={setStep(4)} />}
+            {step === 4 && (
+                <CodeCheckResult cancel={setStep(0)} next={setStep(0)} />
+            )}
         </>
     );
 };
